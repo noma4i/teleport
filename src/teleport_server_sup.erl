694f0de4a9a9f5bd56f8f9e5e32a03af0f9361bc
Bernard Notarianni <bernard.notarianni@gmail.com>
2016-10-27 11:11:48 +0200

    fix teleport:stop_server/1

--- src/teleport_server_sup.erl
+++ src/teleport_server_sup.erl
@@ -41,7 +41,14 @@ start_server(Name, Config) when is_list(Config) ->
 start_server(_, _) -> erlang:error(badarg).
 
 stop_server(Name) ->
-  supervisor:terminate_child(?MODULE, server_name(Name)).
+  ServerRef = {ranch_listener_sup, server_name(Name)},
+  case supervisor:terminate_child(?MODULE, ServerRef) of
+    ok ->
+      _ = supervisor:delete_child(?MODULE, ServerRef),
+      ranch_server:cleanup_listener_opts(ServerRef);
+    Error ->
+      Error
+  end.
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
