66416ab97dc522fb40d7642f55cd88ca07a35d78
benoitc <bchesneau@gmail.com>
2016-10-29 13:50:45 +0200

    add missing license headers

--- src/teleport_client_sup.erl
+++ src/teleport_client_sup.erl
@@ -1,3 +1,11 @@
+%% Copyright (c) 2016 Contributors as noted in the AUTHORS file
+%%
+%% This file is part teleport
+%%
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -module(teleport_client_sup).
 -author("Benoit Chesneau").
 
